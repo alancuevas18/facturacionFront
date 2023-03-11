db024b8b942010a09b0c634f5359880688630b77
Alan Ramses Cuevas De Jesus <alancuevas18_@hotmail.com>
2023-03-11 09:37:04 -0400

    ajust dashboard

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -5,15 +5,12 @@
       <card type="chart">
         <template slot="header">
           <div class="row">
-            <div class="col-sm-6" >
+            <div class="col-sm-6">
               <h5 class="card-category">Total shipments</h5>
               <h2 class="card-title">Performance</h2>
             </div>
             <div class="col-sm-6 d-flex d-sm-block">
-              <div
-                class="btn-group btn-group-toggle"
-                data-toggle="buttons"
-              >
+              <div class="btn-group btn-group-toggle" data-toggle="buttons">
                 <label
                   v-for="(option, index) in bigLineChartCategories"
                   :key="option.name"
@@ -38,6 +35,7 @@
           </div>
         </template>
         <div class="chart-area">
+          <!-- Linea de Chart -->
           <line-chart
             style="height: 100%"
             ref="bigChart"
@@ -63,12 +61,12 @@
     </div>
 
     <!-- Small charts -->
-    <div class="col-lg-4">
+    <!-- <div class="col-lg-4">
       <card type="chart">
         <template slot="header">
           <h5 class="card-category">Total Shipments</h5>
           <h3 class="card-title">
-            <i class="tim-icons icon-bell-55 text-primary "></i> 763,215
+            <i class="tim-icons icon-bell-55 text-primary"></i> 763,215
           </h3>
         </template>
         <div class="chart-area">
@@ -88,7 +86,7 @@
         <template slot="header">
           <h5 class="card-category">Daily Sales</h5>
           <h3 class="card-title">
-            <i class="tim-icons icon-delivery-fast text-info "></i> 3,500€
+            <i class="tim-icons icon-delivery-fast text-info"></i> 3,500€
           </h3>
         </template>
         <div class="chart-area">
@@ -107,7 +105,7 @@
         <template slot="header">
           <h5 class="card-category">Completed tasks</h5>
           <h3 class="card-title">
-            <i class="tim-icons icon-send text-success "></i> 12,100K
+            <i class="tim-icons icon-send text-success"></i> 12,100K
           </h3>
         </template>
         <div class="chart-area">
@@ -120,7 +118,8 @@
           </line-chart>
         </div>
       </card>
-    </div>
+    </div> -->
+    <!--END Small charts -->
     <div class="col-lg-5">
       <card type="tasks">
         <template slot="header">
@@ -152,21 +151,34 @@
   </div>
 </template>
 <script>
-import LineChart from '@/components/Charts/LineChart';
-import BarChart from '@/components/Charts/BarChart';
-import * as chartConfigs from '@/components/Charts/config';
-import TaskList from './TaskList';
-import UserTable from './UserTable';
-import CountryMapCard from './CountryMapCard';
-import StatsCard from 'src/components/Cards/StatsCard';
-import config from '@/config';
+import LineChart from '@/components/Charts/LineChart'
+import BarChart from '@/components/Charts/BarChart'
+import * as chartConfigs from '@/components/Charts/config'
+import TaskList from './TaskList'
+import UserTable from './UserTable'
+import StatsCard from 'src/components/Cards/StatsCard'
+import config from '@/config'
 
 let bigChartData = [
   [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
   [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
-  [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
+  [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130],
+  [130, 130, 130, 130, 130, 130, 130, 130, 130, 130, 160]
 ]
-let bigChartLabels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+let bigChartLabels = [
+  'JAN',
+  'FEB',
+  'MAR',
+  'APR',
+  'MAY',
+  'JUN',
+  'JUL',
+  'AUG',
+  'SEP',
+  'OCT',
+  'NOV',
+  'DEC'
+]
 let bigChartDatasetOptions = {
   fill: true,
   borderColor: config.colors.primary,
@@ -179,7 +191,7 @@ let bigChartDatasetOptions = {
   pointBorderWidth: 20,
   pointHoverRadius: 4,
   pointHoverBorderWidth: 15,
-  pointRadius: 4,
+  pointRadius: 4
 }
 
 export default {
@@ -188,7 +200,6 @@ export default {
     BarChart,
     StatsCard,
     TaskList,
-    CountryMapCard,
     UserTable
   },
   data() {
@@ -226,122 +237,128 @@ export default {
       bigLineChart: {
         activeIndex: 0,
         chartData: {
-          datasets: [{
-            ...bigChartDatasetOptions,
-            data: bigChartData[0]
-          }],
+          datasets: [
+            {
+              ...bigChartDatasetOptions,
+              data: bigChartData[0]
+            }
+          ],
           labels: bigChartLabels
         },
         extraOptions: chartConfigs.purpleChartOptions,
         gradientColors: config.colors.primaryGradient,
         gradientStops: [1, 0.4, 0],
         categories: []
-      },
-      purpleLineChart: {
-        extraOptions: chartConfigs.purpleChartOptions,
-        chartData: {
-          labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-          datasets: [
-            {
-              label: 'Data',
-              fill: true,
-              borderColor: config.colors.primary,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.primary,
-              pointBorderColor: 'rgba(255,255,255,0)',
-              pointHoverBackgroundColor: config.colors.primary,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [80, 100, 70, 80, 120, 80]
-            }
-          ]
-        },
-        gradientColors: config.colors.primaryGradient,
-        gradientStops: [1, 0.2, 0]
-      },
-      greenLineChart: {
-        extraOptions: chartConfigs.greenChartOptions,
-        chartData: {
-          labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
-          datasets: [
-            {
-              label: 'My First dataset',
-              fill: true,
-              borderColor: config.colors.danger,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.danger,
-              pointBorderColor: 'rgba(255,255,255,0)',
-              pointHoverBackgroundColor: config.colors.danger,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [90, 27, 60, 12, 80]
-            }
-          ]
-        },
-        gradientColors: [
-          'rgba(66,134,121,0.15)',
-          'rgba(66,134,121,0.0)',
-          'rgba(66,134,121,0)'
-        ],
-        gradientStops: [1, 0.4, 0]
-      },
-      blueBarChart: {
-        extraOptions: chartConfigs.barChartOptions,
-        chartData: {
-          labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-          datasets: [
-            {
-              label: 'Countries',
-              fill: true,
-              borderColor: config.colors.info,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              data: [53, 20, 10, 80, 100, 45]
-            }
-          ]
-        },
-        gradientColors: config.colors.primaryGradient,
-        gradientStops: [1, 0.4, 0]
       }
-    };
+      // purpleLineChart: {
+      //   extraOptions: chartConfigs.purpleChartOptions,
+      //   chartData: {
+      //     labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+      //     datasets: [
+      //       {
+      //         label: 'Data',
+      //         fill: true,
+      //         borderColor: config.colors.primary,
+      //         borderWidth: 2,
+      //         borderDash: [],
+      //         borderDashOffset: 0.0,
+      //         pointBackgroundColor: config.colors.primary,
+      //         pointBorderColor: 'rgba(255,255,255,0)',
+      //         pointHoverBackgroundColor: config.colors.primary,
+      //         pointBorderWidth: 20,
+      //         pointHoverRadius: 4,
+      //         pointHoverBorderWidth: 15,
+      //         pointRadius: 4,
+      //         data: [80, 100, 70, 80, 120, 80]
+      //       }
+      //     ]
+      //   },
+      //   gradientColors: config.colors.primaryGradient,
+      //   gradientStops: [1, 0.2, 0]
+      // },
+      // greenLineChart: {
+      //   extraOptions: chartConfigs.greenChartOptions,
+      //   chartData: {
+      //     labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
+      //     datasets: [
+      //       {
+      //         label: 'My First dataset',
+      //         fill: true,
+      //         borderColor: config.colors.danger,
+      //         borderWidth: 2,
+      //         borderDash: [],
+      //         borderDashOffset: 0.0,
+      //         pointBackgroundColor: config.colors.danger,
+      //         pointBorderColor: 'rgba(255,255,255,0)',
+      //         pointHoverBackgroundColor: config.colors.danger,
+      //         pointBorderWidth: 20,
+      //         pointHoverRadius: 4,
+      //         pointHoverBorderWidth: 15,
+      //         pointRadius: 4,
+      //         data: [90, 27, 60, 12, 80]
+      //       }
+      //     ]
+      //   },
+      //   gradientColors: [
+      //     'rgba(66,134,121,0.15)',
+      //     'rgba(66,134,121,0.0)',
+      //     'rgba(66,134,121,0)'
+      //   ],
+      //   gradientStops: [1, 0.4, 0]
+      // }
+      // blueBarChart: {
+      //   extraOptions: chartConfigs.barChartOptions,
+      //   chartData: {
+      //     labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
+      //     datasets: [
+      //       {
+      //         label: 'Countries',
+      //         fill: true,
+      //         borderColor: config.colors.info,
+      //         borderWidth: 2,
+      //         borderDash: [],
+      //         borderDashOffset: 0.0,
+      //         data: [53, 20, 10, 80, 100, 45]
+      //       }
+      //     ]
+      //   },
+      //   gradientColors: config.colors.primaryGradient,
+      //   gradientStops: [1, 0.4, 0]
+      // }
+    }
   },
   computed: {
-
     bigLineChartCategories() {
-      return [{ name: 'Accounts', icon: 'tim-icons icon-single-02' }, { name: 'Purchases', icon: 'tim-icons icon-gift-2' }, { name: 'Sessions', icon: 'tim-icons icon-tap-02' }];
+      return [
+        { name: 'Accounts', icon: 'tim-icons icon-single-02' },
+        { name: 'Purchases', icon: 'tim-icons icon-gift-2' },
+        { name: 'Sessions', icon: 'tim-icons icon-tap-02' },
+        { name: 'allin', icon: 'tim-icons icon-tap-02' }
+      ]
     }
   },
   methods: {
     initBigChart(index) {
       let chartData = {
-        datasets: [{
-          ...bigChartDatasetOptions,
-          data: bigChartData[index]
-        }],
+        datasets: [
+          {
+            ...bigChartDatasetOptions,
+            data: bigChartData[index]
+          }
+        ],
         labels: bigChartLabels
-      };
-      this.$refs.bigChart.updateGradients(chartData);
-      this.bigLineChart.chartData = chartData;
-      this.bigLineChart.activeIndex = index;
+      }
+      this.$refs.bigChart.updateGradients(chartData)
+      this.bigLineChart.chartData = chartData
+      this.bigLineChart.activeIndex = index
     }
   },
   mounted() {
-    this.i18n = this.$i18n;
+    this.i18n = this.$i18n
 
-    this.initBigChart(0);
+    this.initBigChart(0)
   },
-  beforeDestroy() {
-
-  }
-};
+  beforeDestroy() {}
+}
 </script>
 <style></style>
