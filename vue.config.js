bf4d17c1d9056b75226486d29999876d8e2dcab0
eliezer martinez <eliezermartinez1996@hotmail.com>
2023-05-23 20:28:51 -0400

    icon

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
     ]
   },
   pwa: {
-    name: 'FacturacionEMAC',
+    name: 'Facturacion Emac',
     themeColor: '#344675',
     msTileColor: '#344675',
     appleMobileWebAppCapable: 'yes',
